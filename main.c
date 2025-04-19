a616e014bf9d2b22620dbe72ef825b2f21bfab36
SAMUEL SERBIM SOARES <125083598+SSerbS@users.noreply.github.com>
2025-04-18 23:58:02 -0300

    Catching errors

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#define _CRT_SECURE_NO_WARNING
+
 void exibeMenu() {
     printf("===============================\n");
     printf("   Calculadora Simples\n");
@@ -15,32 +15,35 @@ void exibeMenu() {
 int getOpcao() {
     int op = 0;
     scanf_s(" %d", &op);
-    if(op < 1 || op > 5){
+    if (op < 1 || op > 5) {
         do { printf("Por favor insira um valor entre 1 e 5: "); scanf_s(" %d", &op); } while (op < 1 || op > 5);
     }
     return op;
 }
 
 void doOperacoes(int op) {
-    int num1 = 0;
-    int num2 = 0;
+    float num1 = 0;
+    float num2 = 0;
     printf("Digite o primeiro número: ");
-    scanf_s("%d", &num1);
+    scanf_s(" %f", &num1);
     printf("Digite o segundo número: ");
-    scanf_s("%d", &num2);
+    scanf_s(" %f", &num2);
+    if (num2 == 0) {
+        do { printf("Digite um segundo número diferente de zero: "); scanf_s("%f", &num2); } while (num2 == 0);
+    }
 
     switch (op) {
     case 1:
-        printf("Resultado: %d + %d = %d\n", num1, num2, num1 + num2);
+        printf("Resultado: %f + %f = %f\n", num1, num2, num1 + num2);
         break;
     case 2:
-        printf("Resultado: %d - %d = %d\n", num1, num2, num1 - num2);
+        printf("Resultado: %f - %f = %f\n", num1, num2, num1 - num2);
         break;
     case 3:
-        printf("Resultado: %d * %d = %d\n", num1, num2, num1 * num2);
+        printf("Resultado: %f * %f = %f\n", num1, num2, num1 * num2);
         break;
     case 4:
-        printf("Resultado: %d / %d = %d\n", num1, num2, num1 / num2);
+        printf("Resultado: %f / %f = %f\n", num1, num2, num1 / num2);
         break;
     default:
         break;
@@ -53,9 +56,9 @@ void doOperacoes(int op) {
 bool rodaDenovo() {
     char sn;
     printf("Deseja realizar outra operação? (s/n): ");
-    scanf_s("%c", &sn);
-    do { printf("Escreva 's' ou 'n': "); scanf_s("%c", &sn); } while ((sn != 's') && (sn != 'n'));
-    if (sn == 's') {
+    sn = getchar();
+    do { printf("Escreva 's' ou 'n': "); sn = getchar(); } while (((sn != 's') && (sn != 'n') && (sn != 'S') && (sn != 'N')));
+    if (sn == 's' || sn == 'S') {
         return 1;
     }
     else { return 0; }
